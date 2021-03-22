7877226e15944a000a9e1ccaf62e2ab4ce488cbf
Andrea Esposito <andexpox@gmail.com>
2021-03-22 15:44:19 +0100

    alert

--- mail/js/script.js
+++ mail/js/script.js
@@ -5,8 +5,18 @@ var mailUtente = prompt("Scrivi la tua email");
 
 // Lista delle mail che possono accedere
 var mailConsentite = ["ludovicojamboni@gmail.com", "gianframmarconi@yahoo.com", "scapillazzuli@outlook.com", "wildjohnny@rocketmail.com", "trefrizzacchio@libero.it"];
+var emailConfermata = false;
 
 for (var i = 0; i < mailConsentite.length; i++) {
-    console.log()];
-    
+    var emailGiusta = mailConsentite[i];
+
+    if (emailGiusta == mailUtente) {
+        emailConfermata = true;
+    }  
+}
+
+if (emailConfermata == true) {
+    alert("Email Valida");
+} else {
+    alert("Email Non Valida");
 }
