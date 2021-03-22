6a40b1fb0dbba556c4e840673a0c875013483e6b
Andrea Esposito <andexpox@gmail.com>
2021-03-22 15:29:22 +0100

    wip

---
+++ mail/js/script.js
@@ -0,0 +1,12 @@
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// Mail Utente
+var mailUtente = prompt("Scrivi la tua email");
+
+// Lista delle mail che possono accedere
+var mailConsentite = ["ludovicojamboni@gmail.com", "gianframmarconi@yahoo.com", "scapillazzuli@outlook.com", "wildjohnny@rocketmail.com", "trefrizzacchio@libero.it"];
+
+for (var i = 0; i < mailConsentite.length; i++) {
+    console.log()];
+    
+}
